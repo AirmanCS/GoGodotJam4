3980a865fd4294f660f4ea82ac54d9d99f1a4abe
Fabián Cárdenas Sancho <fabian_air@outlook.com>
2023-05-10 19:04:04 -0600

    setting up new branch

--- assets/scripts/obstacle_generator.gd
+++ assets/scripts/obstacle_generator.gd
@@ -1,16 +1,17 @@
 extends Node3D
 
 @export var objects: Array[Mesh]
-@onready var mesh = objects[0]
+
 @onready var newRigid = RigidBody3D.new()
 @onready var mesh_instance = MeshInstance3D.new()
 @onready var coll_shape = CollisionShape3D.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var max_size = objects.size()-1
+	var mesh = objects[randi_range(0, max_size)]
 	add_child(newRigid)
 	mesh_instance.mesh = mesh
 	newRigid.add_child(mesh_instance)
 	coll_shape.shape = mesh.create_convex_shape()
-
 	newRigid.add_child(coll_shape)
