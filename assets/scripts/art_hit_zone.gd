562893c27b2531c36bc634728b60cc66c746a8cf
Fabián Cárdenas Sancho <fabian_air@outlook.com>
2023-05-14 02:05:48 -0600

    arte

--- assets/scripts/art_hit_zone.gd
+++ assets/scripts/art_hit_zone.gd
@@ -1,6 +1,12 @@
 extends Area3D
 
+@onready var animation_player: AnimationPlayer = $Painting/AnimationPlayer
+
+
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("obstacles"):
 		GameManager.broken_art_counter()
-		queue_free()
+		animation_player.play("fall")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	queue_free()
